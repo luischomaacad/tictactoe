1789b3fa1ecd617d38bfaac18ecb370b3804b344
Luis Choma <luis.choma@acad.pucrs.br>
2018-09-10 21:44:08 -0300

    Corzinha bonita e fix no empate

--- src/board.js
+++ src/board.js
@@ -10,7 +10,7 @@ class Board extends Component {
       turn: 'X',
       gameEnded: false,
       board: Array(9).fill(''),
-      totalMoves: 0,
+      totalMoves: 1,
       winner: ''
     }
   }
@@ -21,11 +21,12 @@ class Board extends Component {
         this.setState({
           turn: this.state.turn === 'X' ? 'O' : 'X',
           board: this.state.board,
-          totalMoves: this.state.totalMoves++
+          totalMoves: this.state.totalMoves+1
         })
         console.log(this.state.board)
+        console.log(this.state.totalMoves)
       } else {
-        console.log("n pode jogar aí o anta")
+        console.log("Jogada inválida")
       }
       let result = this.checkMoves();
       this.checkWinner(result) 
@@ -39,26 +40,26 @@ class Board extends Component {
           return board[moves[i][0]];
     }
     
-    if(this.totalMoves == 9) {
+    if(this.state.totalMoves == 9) {
       return 'draw';
     }
   }
   
   checkWinner(result) {
     if(result == 'X') {
-      console.log('Acabou e o Xizao ganhou')
+      console.log('Acabou e o X ganhou!')
       this.gameEnded = true;
       this.setState({
         winner: 'X'
-      });
+      })
     } else if(result == 'O') {
-      console.log('Acabou e o Bolao ganhou')
+      console.log('Acabou e o O ganhou!')
       this.gameEnded = true;
       this.setState({
         winner: 'O'
-      });
+      })
     } else if(result == 'draw') {
-      console.log('Dois bosta jogando')
+      console.log('Empate!')
       this.gameEnded = true;
       this.setState({
         winner: 'draw'
@@ -70,8 +71,9 @@ class Board extends Component {
   renderSquares () {
     let squares = []
     for(let i = 0; i<9; i++){
+      let idName = (`square-${i}`)
       squares.push(
-        <div key={i} className="square" onClick={() =>this.move(i)}>
+        <div key={i} id={idName} className="square" onClick={() =>this.move(i)}>
           {this.state.board[i]}
         </div>
       )

752c92ab10210bf2e44266de860541d380c24973
Luis Choma <luis.choma@acad.pucrs.br>
2018-09-24 19:55:42 -0300

    desperate one

--- src/board.js
+++ src/board.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './stylesheets/board.css';
 
 class Board extends Component {
-  
+
   constructor() {
     super()
     this.state = {
@@ -16,17 +16,17 @@ class Board extends Component {
   }
   
   move = (i) => {
-      if(this.state.board[i] === ''){
+      if(this.state.board[i] === '' && !this.state.gameEnded){
         this.state.board[i] = this.state.turn
         this.setState({
           turn: this.state.turn === 'X' ? 'O' : 'X',
           board: this.state.board,
           totalMoves: this.state.totalMoves+1
         })
-        console.log(this.state.board)
-        console.log(this.state.totalMoves)
+        // console.log(this.state.board)
+        // console.log(this.state.totalMoves)
       } else {
-        console.log("Jogada inválida")
+        // console.log("Jogada inválida")
       }
       let result = this.checkMoves();
       this.checkWinner(result) 
@@ -47,21 +47,21 @@ class Board extends Component {
   
   checkWinner(result) {
     if(result == 'X') {
-      console.log('Acabou e o X ganhou!')
-      this.gameEnded = true;
+      // console.log('Acabou e o X ganhou!')
       this.setState({
+        gameEnded: true,
         winner: 'X'
       })
     } else if(result == 'O') {
-      console.log('Acabou e o O ganhou!')
-      this.gameEnded = true;
+      // console.log('Acabou e o O ganhou!')
       this.setState({
+        gameEnded: true,
         winner: 'O'
       })
     } else if(result == 'draw') {
-      console.log('Empate!')
-      this.gameEnded = true;
+      // console.log('Empate!')
       this.setState({
+        gameEnded: true,
         winner: 'draw'
       })
     }
@@ -74,7 +74,7 @@ class Board extends Component {
       let idName = (`square-${i}`)
       squares.push(
         <div key={i} id={idName} className="square" onClick={() =>this.move(i)}>
-          {this.state.board[i]}
+          <p className="square-symbol">{this.state.board[i]}</p>
         </div>
       )
     }
@@ -83,14 +83,46 @@ class Board extends Component {
     )
   }
   
+  renderWinner() {
+    let winner = this.state.winner
+    let message = 'O melhor jogo da velha do mundo'
+    if(this.state.gameEnded && winner == 'draw') {
+      message = 'O jogo empatou'
+    } else if (this.state.gameEnded){
+      message = `O jogo acabou e o vencedor é ${this.state.winner}`
+    }
+    
+    return(
+      <div className="winner-title">
+        <h1> {message} </h1>
+      </div>
+    )
+  }
+  
+  resetGame() {
+    this.setState({
+      turn: 'X',
+      gameEnded: false,
+      board: Array(9).fill(''),
+      totalMoves: 1,
+      winner: ''
+    })
+  }
+  
+  
   render() {
+    console.log(JSON.stringify(this.state))
     return (
       <React.Fragment>
+        {this.renderWinner()}      
         <div className="board-container">
           <div className="board">
             {this.renderSquares()}
           </div>
         </div>
+        <div className="reset-button-container">
+          <button className="reset-button" onClick = {this.resetGame.bind(this)}>Reiniciar o jogo</button>  
+        </div>    
       </React.Fragment>
         
     );
